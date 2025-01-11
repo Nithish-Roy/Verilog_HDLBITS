2735253d8b4688f55dc5564157284041e61f1147
Nithish Roy <roynithish70@gmail.com>
2025-01-11 21:27:42 +0530

    Create popcount3.v

---
+++ Hdl_Bits/circuits/combinational_logic/Basic_Gates/popcount3.v
@@ -0,0 +1,13 @@
+module top_module( 
+    input [2:0] in,
+    output [1:0] out );
+    integer i;
+    always @(*) begin 
+            out = 0;
+            for(i = 0; i<3; i++) begin
+                    if (in[i] == 1'b1)begin
+                        out = out + in [i];
+                    end 
+                end
+        	end                       
+endmodule
